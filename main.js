9ae9339c02e4601f7ab9cd55fc876f1f49916637
simjaesung <89987940+simjaesung@users.noreply.github.com>
2022-02-01 15:36:14 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+        navigation:true,
+        navigationPosition:'right',
+        navigationTooltips:['HOME','INFORMATION','GOODS','CONTACT'],
+        showActiveTooltip: true,
+
+    
+
+	});
+
+});
